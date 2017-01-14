a5c4c41539b78f1a83d41c68346463013d06b8fb
Philip Dai <smartielion@gmail.com>
2017-01-14 08:21:37 -0500

    Fix stock.service failed unit test

--- src/app/services/stock.service.spec.ts
+++ src/app/services/stock.service.spec.ts
@@ -2,15 +2,17 @@
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { StockService } from './stock.service';
+import { HttpModule } from '@angular/http';
 
 describe('StockService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [StockService]
+      providers: [StockService],
+      imports: [HttpModule]
     });
   });
 
-  // it('should ...', inject([StockService], (service: StockService) => {
-  //   expect(service).toBeTruthy();
-  // }));
+  it('should ...', inject([StockService], (service: StockService) => {
+    expect(service).toBeTruthy();
+  }));
 });

dd3cdc368f2a89d6e54cb464e0eae2dc355cc63b
Philip Dai <smartielion@gmail.com>
2017-01-14 08:03:38 -0500

    Fix mutualfunds.component.spec failed unit test

--- src/app/mutualfunds/mutualfunds.component.spec.ts
+++ src/app/mutualfunds/mutualfunds.component.spec.ts
@@ -22,7 +22,7 @@ describe('MutualfundsComponent', () => {
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });

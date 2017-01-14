02ebb5fe9b8d51aad95e2d9b775bcb1e99cdee87
Philip Dai <smartielion@gmail.com>
2017-01-14 08:18:22 -0500

    Fix stock-filter.pipe failed unit test

--- src/app/pipes/stock-filter.pipe.spec.ts
+++ src/app/pipes/stock-filter.pipe.spec.ts
@@ -4,8 +4,8 @@ import { TestBed, async } from '@angular/core/testing';
 import { StockFilterPipe } from './stock-filter.pipe';
 
 describe('StockFilterPipe', () => {
-  // it('create an instance', () => {
-  //   let pipe = new StockFilterPipe();
-  //   expect(pipe).toBeTruthy();
-  // });
+  it('create an instance', () => {
+    let pipe = new StockFilterPipe();
+    expect(pipe).toBeTruthy();
+  });
 });

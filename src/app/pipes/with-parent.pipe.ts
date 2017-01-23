1bb544f9a58016eb89d16e8503735559c08a43c3
Philip Dai <smartielion@gmail.com>
2017-01-22 23:28:40 -0500

    Ajust create stock modal change ngModel from two way data binding to one way data binding

--- src/app/pipes/with-parent.pipe.ts
+++ src/app/pipes/with-parent.pipe.ts
@@ -7,7 +7,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class WithParentPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    return value.map(t=> {
+    return value.map( t => {
       return {
         item: t,
         parent: value
